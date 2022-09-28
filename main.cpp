a1e4eb697dbc07e23c00bf05bf9c3690006ad9ff
mathias.renoult <mathias.renoult@protonmail.ch>
2022-09-28 17:22:32 +0200

    Added functions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
+
 /*
  * Efficient way to check if a player won a tic tac toe game.
  * The input is a 3x3 array of bool. NULL is the default state
  * and true/false are for the players.
  */
 const int SIZE = 3;
+
+void PrintArray(int array[][SIZE])
+{
+    for(int i = 0; i<SIZE; i++)
+    {
+        for(int j = 0; j<SIZE;j++)
+        {
+            if(array[i][j] == -1) std::cout << "X";
+            else if(array[i][j] == 1) std::cout << "O";
+            else std::cout << "-";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void GenerateArray(int array[][SIZE])
+{
+    srand(time(0));
+    for(int i = 0; i<SIZE; i++)
+    {
+        for(int j = 0; j<SIZE;j++)
+        {
+            array[i][j] = (rand()%3) - 1;
+        }
+    }
+}
+
 int main() {
     int b[SIZE][SIZE] =
             {
@@ -13,17 +42,8 @@ int main() {
                     {0,-1,0},
                     {-1,0,0},
             };
-    srand(time(0));
-    // generate random inputs
-    /*
-    for(int i = 0; i<SIZE; i++)
-    {
-        for(int j = 0; j<SIZE;j++)
-        {
-            b[i][j] = (rand()%3) - 1;
-        }
-    }
-     */
+
+    //GenerateArray(b);
 
     int row = 0,column = 0,diagonal1 = 0, diagonal2 = 0;
     for(int i=0;i<SIZE;i++)
@@ -55,19 +75,8 @@ int main() {
         row = 0;
         column = 0;
     }
-    std::cout << diagonal2 << std::endl;
 
-    // print array
-    for(int i = 0; i<SIZE; i++)
-    {
-        for(int j = 0; j<SIZE;j++)
-        {
-            if(b[i][j] == -1) std::cout << "X";
-            else if(b[i][j] == 1) std::cout << "O";
-            else std::cout << "-";
-        }
-        std::cout << std::endl;
-    }
+    PrintArray(b);
 
     return 0;
 }
